a6cb3348c70195f52b63466d86cc5a237f66dc59
onspace.ai <onspace.ai@gmail.com>
2026-07-20 17:26:00 -0400

    Upgrade QuickButlerBar + Butler AI Header

--- metro.config.js
+++ metro.config.js
@@ -7,7 +7,7 @@ try { require('./tools/postinstall.js'); } catch (e) {}
 const config = getDefaultConfig(__dirname);
 
 // Bump cache to force full rebuild.
-config.cacheVersion = 'butler-ai-v5.0.45-clean106';
+config.cacheVersion = 'butler-ai-v5.0.46-clean107';
 
 // ── COPYRIGHT NOTICE SERIALIZER ───────────────────────────────────────
 // Prepends a copyright banner to the COMPILED bundle. This banner
